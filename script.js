9c97fdb17d7d3e5321838d718e7f643ddbfcf865
Monika803-ops <monikanv175@gmail.com>
2024-11-27 21:16:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const facts = [
-    "I enjoy building connections with people from various backgrounds.",
-    "I believe in Krishna and Hanumanji.",
-    "I am deeply spiritual.",
-    "I value diversity and friendships.",
-    "I am passionate about learning and self-improvement."
+    "I enjoy making friends from different places.It helps me learn about new cultures and experience divesity.",
+    "I believe in Krishna and Hanumanji and draw spirutual strength frome these beliefs.",
+    "I am  spiritual person and find peace in my connection with God and my beliefs.",
+    "I value independence and self- improvement.",
+    "I focus on my own path and don't worry about what others think of me.",
+    "I can laugh through tough moments.",
+    "I turn negativity into ambition.",
+    "I believe in turning pain into strength.",
+    "I have the right to work, but never to the fruit of work.",
+    "I am the master of my destiny."
 ];
 
 document.getElementById('generateFact').addEventListener('click', function() {
